9f5306db5d2c3884a207c02bda47de34ce232fde
Takuya OHASHI <tky.ohashi@gmail.com>
2018-02-25 22:33:00 +0900

    parse INES header
     - magic code
     - program size
     - character size

--- Sources/SwiftNes/NesFile.swift
+++ Sources/SwiftNes/NesFile.swift
@@ -5,29 +5,33 @@
 //
 import Foundation
 
-fileprivate let headerSize = 4
-fileprivate func checkHeader(_ header: Data) -> Bool {
-	return (header[0] == 0x4E &&
-		header[1] == 0x45 &&
-		header[2] == 0x53 &&
-		header[3] == 0x1A)
+fileprivate let headerSize = 16
+fileprivate let magicSize = 4
+
+// MagicCode is placed in top of header
+fileprivate func checkMagic(_ magic: Data) -> Bool {
+	return (magic[0] == 0x4E &&
+            magic[1] == 0x45 &&
+            magic[2] == 0x53 &&
+            magic[3] == 0x1A)
 }
 
 struct NesFile {
 	let Program: Data
 	let Character: Data
 
-	let programSizeUnit = 16 * 1024
-	let characterSizeUnit = 8 * 1024
+    let programSizeUnit = 16 * 1024
+    let characterSizeUnit = 8 * 1024
 
 	init?(_ data: Data) {
-		guard checkHeader(data.subdata(in: 0..<headerSize)) else {
+		guard checkMagic(data.subdata(in: 0..<magicSize)) else {
 			return nil
 		}
 		let programSize = Int(data[4]) * programSizeUnit
 		let characterSize = Int(data[5]) * characterSizeUnit
 
-		Program = data.subdata(in: 16..<programSize)
-		Character = data.subdata(in: 16..<characterSize)
+        let programBottom = headerSize + programSize
+		Program = data.subdata(in: headerSize..<programBottom)
+		Character = data.subdata(in: programBottom..<(programBottom + characterSize))
 	}
 }
