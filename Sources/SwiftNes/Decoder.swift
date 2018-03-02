6ae18a2c9dbab808ba657d0a643d26575816d44a
Takuya OHASHI <tky.ohashi@gmail.com>
2018-03-02 14:19:45 +0900

    add LDA/LDX instruction

--- Sources/SwiftNes/Decoder.swift
+++ Sources/SwiftNes/Decoder.swift
@@ -8,7 +8,22 @@ import Foundation
 
 struct Decoder {
     let InstructionTable: [Int:Instruction] = [
-        0xA9: Instruction(opcode: .LDA, addressing: .Immediate, bytes: 1, cycle: 1)
+        // LDA: Load to A register
+        0xA9: Instruction(opcode: .LDA, addressing: .Immediate, bytes: 2, cycle: 2),
+        0xA5: Instruction(opcode: .LDA, addressing: .ZeroPage,  bytes: 2, cycle: 3),
+        0xB5: Instruction(opcode: .LDA, addressing: .ZeroPageX, bytes: 2, cycle: 4),
+        0xAD: Instruction(opcode: .LDA, addressing: .Absolute,  bytes: 3, cycle: 4),
+        0xBD: Instruction(opcode: .LDA, addressing: .AbsoluteX, bytes: 3, cycle: 4),
+        0xB9: Instruction(opcode: .LDA, addressing: .AbsoluteY, bytes: 3, cycle: 4),
+        0xA1: Instruction(opcode: .LDA, addressing: .IndirectX, bytes: 2, cycle: 6),
+        0xB1: Instruction(opcode: .LDA, addressing: .IndirectY, bytes: 2, cycle: 5),
+
+        // LDX: Load to X register
+        0xA2: Instruction(opcode: .LDX, addressing: .Immediate, bytes: 2, cycle: 2),
+        0xA6: Instruction(opcode: .LDX, addressing: .ZeroPage,  bytes: 2, cycle: 3),
+        0xB6: Instruction(opcode: .LDX, addressing: .ZeroPageY, bytes: 2, cycle: 4),
+        0xAE: Instruction(opcode: .LDX, addressing: .Absolute,  bytes: 3, cycle: 4),
+        0xBE: Instruction(opcode: .LDX, addressing: .Absolute,  bytes: 3, cycle: 4),
     ]
 
     static func decode() {
