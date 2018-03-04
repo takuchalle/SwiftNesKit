3de4a7c88e123c3485c07510959211a4f2756c8c
Takuya OHASHI <tky.ohashi@gmail.com>
2018-03-04 12:31:59 +0900

    create SwiftNesDissemble

--- Sources/SwiftNesDisassemble/main.swift
+++ Sources/SwiftNesDisassemble/main.swift
@@ -3,6 +3,29 @@
 //
 // Copyright © 2018 Takuya OHASHI. All rights reserved.
 //
+import Foundation
 import SwiftNes
 
-print("disassemble .nes file")
+func usage() {
+    print("Usage: SwiftNesDisassemble [input nes file]")
+}
+
+let arguments = CommandLine.arguments
+let arg1 = arguments.dropFirst().first
+
+guard let path = arg1 else {
+    usage()
+    exit(EXIT_FAILURE)
+}
+
+guard let data = FileManager.default.contents(atPath: path) else {
+    print("Failed to Open \(path)")
+    exit(EXIT_FAILURE)
+}
+
+guard let nesfile = NesFile(data) else {
+    print("Failed to Initialize nesfile")
+    exit(EXIT_FAILURE)
+}
+
+nesfile.dumpAll()
