38818dee7a4d7705dde50b2ad7af807c179bb6d1
Takuya OHASHI <tky.ohashi@gmail.com>
2018-03-04 18:56:47 +0900

    fix indent

--- Tests/SwiftNesTests/NesFileTests.swift
+++ Tests/SwiftNesTests/NesFileTests.swift
@@ -5,7 +5,7 @@ class NesFileTests: XCTestCase {
     func testNesFileInitializeWithWrongHeader() {
         let fileName = "WrongHeader.nes"
 
-	guard let data = openFile(with: fileName) else {
+        guard let data = openFile(with: fileName) else {
             XCTFail("Failed To Open Test File")
             return
         }
