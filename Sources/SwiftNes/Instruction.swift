81a96045fddd75947a85b6e4209133ba73240af6
Takuya OHASHI <tky.ohashi@gmail.com>
2018-03-04 18:51:36 +0900

    change Instruction enum type

--- Sources/SwiftNes/Instruction.swift
+++ Sources/SwiftNes/Instruction.swift
@@ -5,63 +5,63 @@
 //
 
 struct Instruction {
-    enum Opcode {
-        case LDA
-        case LDX
-        case LDY
-        case STA
-        case STX
-        case STY
-        case TAX
-        case TAY
-        case TSX
-        case TXA
-        case TXS
-        case TYA
-        case ADC
-        case AND
-        case ASL
-        case BIT
-        case CMP
-        case CPX
-        case CPY
-        case DEC
-        case DEX
-        case DEY
-        case EOR
-        case INC
-        case INX
-        case INY
-        case LSR
-        case ORA
-        case ROL
-        case ROR
-        case SBC
-        case PHA
-        case PHP
-        case PLA
-        case PLP
-        case JMP
-        case JSR
-        case RTS
-        case RTI
-        case BCC
-        case BCS
-        case BEQ
-        case BMI
-        case BNE
-        case BPL
-        case BVC
-        case BVS
-        case CLC
-        case CLD
-        case CLI
-        case CLV
-        case SEC
-        case SED
-        case SEI
-        case BRK
-        case NOP
+    enum Opcode: String {
+        case LDA = "LDA"
+        case LDX = "LDX"
+        case LDY = "LDY"
+        case STA = "STA"
+        case STX = "STX"
+        case STY = "STY"
+        case TAX = "TAX"
+        case TAY = "TAY"
+        case TSX = "TSX"
+        case TXA = "TXA"
+        case TXS = "TXS"
+        case TYA = "TYA"
+        case ADC = "ADC"
+        case AND = "AND"
+        case ASL = "ASL"
+        case BIT = "BIT"
+        case CMP = "CMP"
+        case CPX = "CPX"
+        case CPY = "CPY"
+        case DEC = "DEC"
+        case DEX = "DEX"
+        case DEY = "DEY"
+        case EOR = "EOR"
+        case INC = "INC"
+        case INX = "INX"
+        case INY = "INY"
+        case LSR = "LSR"
+        case ORA = "ORA"
+        case ROL = "ROL"
+        case ROR = "ROR"
+        case SBC = "SBC"
+        case PHA = "RHA"
+        case PHP = "PHP"
+        case PLA = "PLA"
+        case PLP = "PLP"
+        case JMP = "JMP"
+        case JSR = "JSR"
+        case RTS = "RTS"
+        case RTI = "RTI"
+        case BCC = "BCC"
+        case BCS = "BCS"
+        case BEQ = "BEQ"
+        case BMI = "BMI"
+        case BNE = "BNE"
+        case BPL = "BPL"
+        case BVC = "BVC"
+        case BVS = "BVS"
+        case CLC = "CLC"
+        case CLD = "CLD"
+        case CLI = "CLI"
+        case CLV = "CLV"
+        case SEC = "SEC"
+        case SED = "SED"
+        case SEI = "SEI"
+        case BRK = "BRK"
+        case NOP = "NOP"
     }
     
     enum Addressing {
