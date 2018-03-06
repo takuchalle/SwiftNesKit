a58886f1321820a2ff3a29cfb8393767be41ae6e
Takuya OHASHI <tky.ohashi@gmail.com>
2018-03-06 14:11:32 +0900

    add UInt16 extension
     - return hex value

---
+++ Sources/SwiftNes/Extension.swift
@@ -0,0 +1,11 @@
+//
+// Extension.swift
+//
+// Copyright © 2018 Takuya OHASHI. All rights reserved.
+//
+
+extension UInt16 {
+    var hex: String {
+        return String(self, radix: 16)
+    }
+}
