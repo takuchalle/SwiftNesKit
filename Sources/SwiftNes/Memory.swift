a9426c6aed895a8b49c1ade5f5f4b142fc88fb1c
Takuya OHASHI <tky.ohashi@gmail.com>
2018-03-19 14:39:09 +0900

    add subscript to Memory struct

--- Sources/SwiftNes/Memory.swift
+++ Sources/SwiftNes/Memory.swift
@@ -15,23 +15,36 @@ struct Memory {
     init() {
         ram = [UInt8](repeating: 0, count: maxMemSize + 1)
     }
+
+    subscript(index: Int) -> UInt8 {
+        get {
+            return read1byte(at: UInt16(index))
+        }
+        set(newValue) {
+            write(at: UInt16(index), value: newValue)
+        }
+    }
+
+    subscript(index: Int) -> UInt16 {
+        return read2byte(at: UInt16(index))
+    }
     
     mutating func load(at address: Int, data: [UInt8]) {
         let last = address + data.count
         ram.replaceSubrange(address...last, with: data)
     }
 
-    func read1byte(at address: UInt16) -> UInt8 {
+    private func read1byte(at address: UInt16) -> UInt8 {
         let _address = Int(address)
         return ram[_address]
     }
 
-    func read2byte(at address: UInt16) -> UInt16 {
+    private func read2byte(at address: UInt16) -> UInt16 {
         let _address = Int(address)
         return (UInt16)(ram[_address]) | (UInt16)(ram[_address + 1]) << 8
     }
 
-    mutating func write(at address: UInt16, value: UInt8) {
+    private mutating func write(at address: UInt16, value: UInt8) {
         let _address = Int(address)
         ram[_address] = value
     }
