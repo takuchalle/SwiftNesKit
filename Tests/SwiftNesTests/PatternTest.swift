a873663e0365fd358fe9ff97cb8dc6ad4aa4b11b
Takuya OHASHI <tky.ohashi@gmail.com>
2018-06-27 08:42:59 +0900

    add empty Pattern Test

---
+++ Tests/SwiftNesTests/PatternTest.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import SwiftNesKit
+
+class PatternTests: XCTestCase {
+    func testPattern() {
+    }
+
+    static var allTests = [
+        ("testPattern", testPattern),
+    ]
+}
